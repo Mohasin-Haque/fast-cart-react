afcce5112f9638e2a953ec4be97c7fbaf44670f7
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-03-16 22:11:27 +0530

    fix - changed class to className

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,22 +3,22 @@ import "../navbar/navbar.css";
 const Navbar = () => {
 return(
     <div>
-        <nav class="navbar-ecom">
-            <a href="../index.html" class="brand-title-ecom">fastcart</a>
-            <a href="#" class="toggle-button-ecom">
-                <span class="bar-ecom"></span>
-                <span class="bar-ecom"></span>
-                <span class="bar-ecom"></span>
+        <nav className="navbar-ecom">
+            <a href="../index.html" className="brand-title-ecom">fastcart</a>
+            <a href="#" className="toggle-button-ecom">
+                <span className="bar-ecom"></span>
+                <span className="bar-ecom"></span>
+                <span className="bar-ecom"></span>
             </a>
-            <div class="search-box-ecom">
+            <div className="search-box-ecom">
                 <input type="search" placeholder="search" />
-                <span class="fa fa-search"></span>
+                <span className="fa fa-search"></span>
             </div>
-            <div class="navbar-links-ecom">
+            <div className="navbar-links-ecom">
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">cart</a></li>
-                    <li><a href="#">wishlist</a></li>
+                    <li><a href="#"><i className="bi bi-heart"></i> wishlist</a></li>
+                    <li><a href="#"><i className="bi bi-cart"></i>cart</a></li>
                 </ul>
             </div>
         </nav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,181 +4,183 @@ const HomePage = () => {
     return(
         <div>
             <main>
-        <section class="section section-main">
-            <img class="image-section"
+        <section className="section section-main">
+            <div>
+            <img className="image-section"
                 src="https://cdn.shortpixel.ai/spai/q_+ret_img+to_webp/https://www.banarasisaree.com/wp-content/uploads/2021/11/slider-home-wedding-2nw.jpg"
                 alt="Image failed to load" role="presentation" />
-            <div class="container">
-                <h3 class="heading heading-ecom">Flat 40% off for neogrammers
+            <div className="container">
+                <h3 className="heading heading-ecom">Flat 40% off for neogrammers
                 </h3>
             </div>
-            <p class="about">
+            <p className="about">
                 Welcome to fastcart. It's a e-commerce app which have a wide range of clothings and apparel. Choose the
                 cotagory to explore more.
             </p>
-            <div class="btn-container">
-                <a class="btn btn-primary" href="./pages/product.html">Women's Wear</a>
-                <a class="btn btn-secondary" href="./pages/menswear.html">Men's Wear</a>
+            <div className="btn-container">
+                <a className="btn btn-primary" href="./pages/product.html">Women's Wear</a>
+                <a className="btn btn-secondary" href="./pages/menswear.html">Men's Wear</a>
+            </div>
             </div>
         </section>
     </main>
-    <section class="featured-section">
-        <h3 class="heading heading-avatar heading-card heading-ecom">Featured</h3>
-        <div class="featured-product">
+    <section className="featured-section">
+        <h3 className="heading heading-avatar heading-card heading-ecom">Featured</h3>
+        <div className="featured-product">
             <section>
-                <div class="card-container">
-                    <div class="image-div">
-                        <div class="overlay-image">
-                            <img class="card-image"
+                <div className="card-container">
+                    <div className="image-div">
+                        <div className="overlay-image">
+                            <img className="card-image"
                                 src="https://saree.b-cdn.net/media/catalog/product/cache/b338d39772b77a6788c692c0fe37ef74/s/a/saci7054a-red-banarasi-silk-woven-saree-with-lace.jpg"
                                 alt="error" />
-                            <div class="card-badge card-badge-ecom">New</div>
+                            <div className="card-badge card-badge-ecom">New</div>
                         </div>
                     </div>
-                    <div class="card-content">
-                        <h2 class="card-heading">Red Banarasi Silk Woven Saree with Lace</h2>
-                        <p class="card-author">by MH Textiles and Sarees</p>
-                        <p class="card-content">
+                    <div className="card-content">
+                        <h2 className="card-heading">Red Banarasi Silk Woven Saree with Lace</h2>
+                        <p className="card-author">by MH Textiles and Sarees</p>
+                        <p className="card-content">
                             Give yourself a traditional look by this traditional banarasi silk saree. Made with pure
                             silk and zari fabrics with lace.
                         </p>
                     </div>
-                    <div class="card-actions">
-                        <button class="card-btn">Buy</button>
-                        <button class="card-btn">Add To Cart</button>
-                        <i class="fas fa-heart"></i>
-                        <i class="fas fa-share-alt"></i>
-                        <i class="fas fa-ellipsis-v"></i>
+                    <div className="card-actions">
+                        <button className="card-btn">Buy</button>
+                        <button className="card-btn">Add To Cart</button>
+                        <i className="fas fa-heart"></i>
+                        <i className="fas fa-share-alt"></i>
+                        <i className="fas fa-ellipsis-v"></i>
                     </div>
                 </div>
             </section>
             <section>
-                <div class="card-container">
-                    <div class="image-div">
-                        <div class="overlay-image">
-                            <img class="card-image"
+                <div className="card-container">
+                    <div className="image-div">
+                        <div className="overlay-image">
+                            <img className="card-image"
                                 src="https://saree.b-cdn.net/media/catalog/product/cache/b338d39772b77a6788c692c0fe37ef74/s/a/saci7057a-orange-banarasi-silk-all-over-woven-saree.jpg"
                                 alt="error" />
-                            <div class="card-badge card-badge-ecom">New</div>
+                            <div className="card-badge card-badge-ecom">New</div>
                         </div>
                     </div>
-                    <div class="card-content">
-                        <h2 class="card-heading">Orange Banarasi Silk All Over Woven Saree</h2>
-                        <p class="card-author">by MH Textiles and Sarees</p>
-                        <p class="card-content">
+                    <div className="card-content">
+                        <h2 className="card-heading">Orange Banarasi Silk All Over Woven Saree</h2>
+                        <p className="card-author">by MH Textiles and Sarees</p>
+                        <p className="card-content">
                             Give yourself a traditional look by this all over woven saree. Made with pure silk and zari
                             fabrics.
                         </p>
                     </div>
-                    <div class="card-actions">
-                        <button class="card-btn">Buy</button>
-                        <button class="card-btn">Add To Cart</button>
-                        <i class="fas fa-heart"></i>
-                        <i class="fas fa-share-alt"></i>
-                        <i class="fas fa-ellipsis-v"></i>
+                    <div className="card-actions">
+                        <button className="card-btn">Buy</button>
+                        <button className="card-btn">Add To Cart</button>
+                        <i className="fas fa-heart"></i>
+                        <i className="fas fa-share-alt"></i>
+                        <i className="fas fa-ellipsis-v"></i>
                     </div>
                 </div>
             </section>
             <section>
-                <div class="card-container">
-                    <div class="image-div">
-                        <div class="overlay-image">
-                            <img class="card-image"
+                <div className="card-container">
+                    <div className="image-div">
+                        <div className="overlay-image">
+                            <img className="card-image"
                                 src="https://saree.b-cdn.net/media/catalog/product/cache/b338d39772b77a6788c692c0fe37ef74/s/a/saci7050a-red-banarasi-silk-traditional-saree.jpg"
                                 alt="error" />
-                            <div class="card-badge card-badge-ecom">New</div>
+                            <div className="card-badge card-badge-ecom">New</div>
                         </div>
                     </div>
-                    <div class="card-content">
-                        <h2 class="card-heading">Red Banarasi Silk Traditional Saree</h2>
-                        <p class="card-author">by MH Textiles and Sarees</p>
-                        <p class="card-content">
+                    <div className="card-content">
+                        <h2 className="card-heading">Red Banarasi Silk Traditional Saree</h2>
+                        <p className="card-author">by MH Textiles and Sarees</p>
+                        <p className="card-content">
                             Give yourself a traditional look by this traditional banarasi silk saree. Made with pure
                             silk
                             and zari fabrics.
                         </p>
                     </div>
-                    <div class="card-actions">
-                        <button class="card-btn">Buy</button>
-                        <button class="card-btn">Add To Cart</button>
-                        <i class="fas fa-heart"></i>
-                        <i class="fas fa-share-alt"></i>
-                        <i class="fas fa-ellipsis-v"></i>
+                    <div className="card-actions">
+                        <button className="card-btn">Buy</button>
+                        <button className="card-btn">Add To Cart</button>
+                        <i className="fas fa-heart"></i>
+                        <i className="fas fa-share-alt"></i>
+                        <i className="fas fa-ellipsis-v"></i>
                     </div>
                 </div>
             </section>
         </div>
     </section>
-    <section class="featured-section">
-        <h3 class="heading heading-avatar heading-card heading-ecom">Category</h3>
-        <div class="featured-product">
+    <section className="featured-section">
+        <h3 className="heading heading-avatar heading-card heading-ecom">Category</h3>
+        <div className="featured-product">
             <section>
-                <div class="card-container card-cotegory">
-                    <div class="card-horizontal">
-                        <div class="image-div">
-                            <div class="overlay-image">
-                                <img class="card-image image-horizontal image-cotegory"
+                <div className="card-container card-cotegory">
+                    <div className="card-horizontal">
+                        <div className="image-div">
+                            <div className="overlay-image">
+                                <img className="card-image image-horizontal image-cotegory"
                                     src="https://www.indiansilkhouseagencies.com/common-new/images/klc/alt-flt2.jpg?v=339"
                                     alt="error" />
-                                <div class="card-badge card-badge-cotegory">Out of Stock</div>
+                                <div className="card-badge card-badge-cotegory">Out of Stock</div>
                             </div>
                         </div>
-                        <div class="card-content">
-                            <h2 class="card-heading">Anokha Suits</h2>
-                            <p class="card-author">by MH Textiles and Sarees</p>
-                            <p class="card-content">
+                        <div className="card-content">
+                            <h2 className="card-heading">Anokha Suits</h2>
+                            <p className="card-author">by MH Textiles and Sarees</p>
+                            <p className="card-content">
                                 Suits for modern girls. Look traditional.
                             </p>
-                            <div class="card-actions">
-                                <a class="btn btn-secondary btn-cotegory" href="./pages/product.html">Explore</a>
+                            <div className="card-actions">
+                                <a className="btn btn-secondary btn-cotegory" href="./pages/product.html">Explore</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <div class="card-container card-cotegory">
-                    <div class="card-horizontal">
-                        <div class="image-div">
-                            <div class="overlay-image">
-                                <img class="card-image image-horizontal image-cotegory"
+                <div className="card-container card-cotegory">
+                    <div className="card-horizontal">
+                        <div className="image-div">
+                            <div className="overlay-image">
+                                <img className="card-image image-horizontal image-cotegory"
                                     src="https://ebxafz3psd.execute-api.us-west-2.amazonaws.com/production/products/SY-SK-412-CR-1.jpg"
                                     alt="error" />
-                                <div class="card-badge card-badge-cotegory">Out of Stock</div>
+                                <div className="card-badge card-badge-cotegory">Out of Stock</div>
                             </div>
                         </div>
-                        <div class="card-content">
-                            <h2 class="card-heading">Sherwani Set</h2>
-                            <p class="card-author">by MH Textiles and Sarees</p>
-                            <p class="card-content">
+                        <div className="card-content">
+                            <h2 className="card-heading">Sherwani Set</h2>
+                            <p className="card-author">by MH Textiles and Sarees</p>
+                            <p className="card-content">
                                 Sherwani for occassions.
                             </p>
-                            <div class="card-actions">
-                                <a class="btn btn-secondary btn-cotegory" href="./pages/menswear.html">Explore</a>
+                            <div className="card-actions">
+                                <a className="btn btn-secondary btn-cotegory" href="./pages/menswear.html">Explore</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <div class="card-container card-cotegory">
-                    <div class="card-horizontal">
-                        <div class="image-div">
-                            <div class="overlay-image">
-                                <img class="card-image image-horizontal image-cotegory"
+                <div className="card-container card-cotegory">
+                    <div className="card-horizontal">
+                        <div className="image-div">
+                            <div className="overlay-image">
+                                <img className="card-image image-horizontal image-cotegory"
                                     src="https://www.indiansilkhouseagencies.com/common-new/images/klc/alt-flt.jpg?v=339"
                                     alt="error" />
-                                <div class="card-badge card-badge-cotegory">Out of Stock</div>
+                                <div className="card-badge card-badge-cotegory">Out of Stock</div>
                             </div>
                         </div>
-                        <div class="card-content">
-                            <h2 class="card-heading">Printed Sarees</h2>
-                            <p class="card-author">by MH Textiles and Sarees</p>
-                            <p class="card-content">
+                        <div className="card-content">
+                            <h2 className="card-heading">Printed Sarees</h2>
+                            <p className="card-author">by MH Textiles and Sarees</p>
+                            <p className="card-content">
                                 Sarees for women. Look traditional.
                             </p>
-                            <div class="card-actions">
-                                <a class="btn btn-secondary btn-cotegory" href="./pages/product.html">Explore</a>
+                            <div className="card-actions">
+                                <a className="btn btn-secondary btn-cotegory" href="./pages/product.html">Explore</a>
                             </div>
                         </div>
                     </div>
