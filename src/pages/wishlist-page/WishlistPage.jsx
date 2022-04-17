a07b74aa2dc8ea91c6cfda49033484d512af3ff6
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-04-17 15:54:31 +0530

    feat : added some styling

--- src/pages/wishlist-page/WishlistPage.jsx
+++ src/pages/wishlist-page/WishlistPage.jsx
@@ -6,30 +6,30 @@ import { emptyWishlist } from "../../assets/index";
 
 const WishlistPage = () => {
 
-    const {wishlistState } = useWishlistContext();
+    const { wishlistState } = useWishlistContext();
 
     return (
         <div>
             <Navbar />
-        <h3 className="heading heading-avatar heading-card heading-ecom">MY WISHLIST : {wishlistState.wishlistData.length} </h3>
-        <div className="cards-center flex-center">
-          {wishlistState.wishlistData.length === 0 ? (
-              <div>
-                  <h2>Nothing here... Go to <Link to="/products" className="colored-text">products</Link> and add some items to wishlist.</h2>
-                  <img src={emptyWishlist} alt="error" />
-              </div>
-          ) : (
-              <div>
-                  {wishlistState.wishlistData.map((product) => {
-                      return (
-                          <div>
-                              <WishlistCard key={product.id} product={product}/>
-                          </div>
-                      )
-                  })}
-              </div>
-          )}
-        </div>
+            <h3 className="heading heading-avatar heading-card heading-ecom">MY WISHLIST : {wishlistState.wishlistData.length} </h3>
+            <div className="cards-center flex-center">
+                {wishlistState.wishlistData.length === 0 ? (
+                    <div>
+                        <h2>Nothing here... Go to <Link to="/products" className="colored-text">products</Link> and add some items to wishlist.</h2>
+                        <img src={emptyWishlist} alt="error" />
+                    </div>
+                ) : (
+                    <div>
+                        {wishlistState.wishlistData.map((product) => {
+                            return (
+                                <div className="wishlist-card">
+                                    <WishlistCard key={product.id} product={product} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
